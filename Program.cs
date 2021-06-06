692933d79e9d0d847e6dcace863c991d17758c2b
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-06 13:51:07 +0300

    Obtain required photo urls

--- Program.cs
+++ Program.cs
@@ -15,16 +15,16 @@ namespace PhotoLinksFromApi
             var httpClient = new HttpClient();
             var httpResponseUser = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
             var httpResponseAlbum = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/albums");
+            var httpResponsePhoto = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/photos");
 
             int dennisSchulistUserId = 0;
+            IEnumerable<int> dennisSchulistAlbumsIds = new int[] { };
 
-           
             if (httpResponseUser.IsSuccessStatusCode)
             {
                 var contentString = await httpResponseUser.Content.ReadAsStringAsync();
                 var users = JsonConvert.DeserializeObject<List<User>>(contentString);
 
-                //users.ForEach(user => Console.WriteLine($"User id: {user.Id}, \r\n Username: {user.Username}, \r\n User e-mail: {user.Email}."));
                 dennisSchulistUserId = users.Where(user => user.Name == "Mrs. Dennis Schulist")
                     .Select(user => user.Id)
                     .FirstOrDefault();
@@ -36,7 +36,8 @@ namespace PhotoLinksFromApi
             {
                 var contentString = await httpResponseAlbum.Content.ReadAsStringAsync();
                 var albums = JsonConvert.DeserializeObject<List<Album>>(contentString);
-                var dennisSchulistAlbumsIds = albums.Where(album => album.UserId == dennisSchulistUserId)
+
+                dennisSchulistAlbumsIds = albums.Where(album => album.UserId == dennisSchulistUserId)
                     .Select(album => album.Id);
                 Console.Write("The id numbers of photo albums by Mrs. Dennis Schulist are: ");
                 foreach (var dennisSchulistAlbumId in dennisSchulistAlbumsIds)
@@ -45,7 +46,21 @@ namespace PhotoLinksFromApi
                 }
 
                 Console.WriteLine();
+            }
 
+            if (httpResponsePhoto.IsSuccessStatusCode)
+            {
+                var contentString = await httpResponsePhoto.Content.ReadAsStringAsync();
+                var photos = JsonConvert.DeserializeObject<List<Photo>>(contentString);
+
+                var dennisSchulistPhotosUrls = photos.Where(photo => dennisSchulistAlbumsIds.Contains(photo.AlbumId))
+                    .Select(photo => photo.Url);
+
+                Console.WriteLine("The urls of photos by Mrs. Dennis Schulist in all her albums are:");
+                foreach (var dennisSchulistPhotoUrl in dennisSchulistPhotosUrls)
+                {
+                    Console.WriteLine(dennisSchulistPhotoUrl);
+                }
             }
         }
     }
