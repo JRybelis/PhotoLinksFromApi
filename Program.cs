d6872e87abd371123090e00a33f0a10d11951aa4
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-06 13:07:58 +0300

    get required album ids

--- Program.cs
+++ Program.cs
@@ -13,47 +13,40 @@ namespace PhotoLinksFromApi
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            //var httpResponsePost = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
             var httpResponseUser = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+            var httpResponseAlbum = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/albums");
 
-            //if (httpResponsePost.IsSuccessStatusCode)
-            //{
-            //    var contentString = await httpResponsePost.Content.ReadAsStringAsync();
-            //    var posts = JsonConvert.DeserializeObject<List<Post>>(contentString);
+            int dennisSchulistUserId = 0;
 
-            //    var filteredPosts = posts.Where(post => post.Id <= 60);
-
-            //    foreach (var post in filteredPosts)
-            //    {
-            //        Console.WriteLine($"Post id: {post.Id}, \r\n Post title: {post.Title}.");
-            //    }
-            //    Console.WriteLine();
-            //}
-
+           
             if (httpResponseUser.IsSuccessStatusCode)
             {
                 var contentString = await httpResponseUser.Content.ReadAsStringAsync();
                 var users = JsonConvert.DeserializeObject<List<User>>(contentString);
 
-                var filteredUsers = users.Where(user => user.Id <= 50);
-
-                foreach (var user in filteredUsers)
-                {
-                    Console.WriteLine($"User id: {user.Id}, \r\n Name: {user.Name}.");
-                }
-
-                Console.WriteLine();
-                users.ForEach(user => Console.WriteLine($"User id: {user.Id}, \r\n Username: {user.Username}, \r\n User e-mail: {user.Email}."));
-                Console.WriteLine();
-
-                var dennisSchulistUserId = users.Where(user => user.Name == "Mrs. Dennis Schulist")
-                    .Select(user => user.Id).FirstOrDefault();
-                    
-                    
-                Console.WriteLine(dennisSchulistUserId);
+                //users.ForEach(user => Console.WriteLine($"User id: {user.Id}, \r\n Username: {user.Username}, \r\n User e-mail: {user.Email}."));
+                dennisSchulistUserId = users.Where(user => user.Name == "Mrs. Dennis Schulist")
+                    .Select(user => user.Id)
+                    .FirstOrDefault();
+                Console.WriteLine("Mrs. Dennis Schulist user id is: " + dennisSchulistUserId);
             }
 
             Console.WriteLine();
+            if (httpResponseAlbum.IsSuccessStatusCode)
+            {
+                var contentString = await httpResponseAlbum.Content.ReadAsStringAsync();
+                var albums = JsonConvert.DeserializeObject<List<Album>>(contentString);
+                var dennisSchulistAlbumsIds = albums.Where(album => album.UserId == dennisSchulistUserId)
+                    .Select(album => album.Id);
+                Console.Write("The id numbers of photo albums by Mrs. Dennis Schulist are: ");
+                foreach (var dennisSchulistAlbumId in dennisSchulistAlbumsIds)
+                {
+                    Console.Write(dennisSchulistAlbumId + " ");
+                }
+
+                Console.WriteLine();
+
+            }
         }
     }
 }
