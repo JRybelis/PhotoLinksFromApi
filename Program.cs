2552fdd88454d5c2e4b38014b15b16c829664af1
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-06 12:30:13 +0300

    get required user id

--- Program.cs
+++ Program.cs
@@ -13,22 +13,22 @@ namespace PhotoLinksFromApi
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
-            var httpResponsePost = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            //var httpResponsePost = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts");
             var httpResponseUser = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
 
-            if (httpResponsePost.IsSuccessStatusCode)
-            {
-                var contentString = await httpResponsePost.Content.ReadAsStringAsync();
-                var posts = JsonConvert.DeserializeObject<List<Post>>(contentString);
+            //if (httpResponsePost.IsSuccessStatusCode)
+            //{
+            //    var contentString = await httpResponsePost.Content.ReadAsStringAsync();
+            //    var posts = JsonConvert.DeserializeObject<List<Post>>(contentString);
 
-                var filteredPosts = posts.Where(post => post.Id <= 60);
+            //    var filteredPosts = posts.Where(post => post.Id <= 60);
 
-                foreach (var post in filteredPosts)
-                {
-                    Console.WriteLine($"Post id: {post.Id}, \r\n Post title: {post.Title}.");
-                }
-                Console.WriteLine();
-            }
+            //    foreach (var post in filteredPosts)
+            //    {
+            //        Console.WriteLine($"Post id: {post.Id}, \r\n Post title: {post.Title}.");
+            //    }
+            //    Console.WriteLine();
+            //}
 
             if (httpResponseUser.IsSuccessStatusCode)
             {
@@ -39,14 +39,18 @@ namespace PhotoLinksFromApi
 
                 foreach (var user in filteredUsers)
                 {
-                    Console.WriteLine($"User id: {user.Id}, \r\n Username: {user.Username}.");
+                    Console.WriteLine($"User id: {user.Id}, \r\n Name: {user.Name}.");
                 }
 
                 Console.WriteLine();
                 users.ForEach(user => Console.WriteLine($"User id: {user.Id}, \r\n Username: {user.Username}, \r\n User e-mail: {user.Email}."));
                 Console.WriteLine();
-                var SamanthaUser = users.Where(user => user.Username == "Samantha").Select(user => user.Username).FirstOrDefault();
-                Console.WriteLine(SamanthaUser);
+
+                var dennisSchulistUserId = users.Where(user => user.Name == "Mrs. Dennis Schulist")
+                    .Select(user => user.Id).FirstOrDefault();
+                    
+                    
+                Console.WriteLine(dennisSchulistUserId);
             }
 
             Console.WriteLine();
